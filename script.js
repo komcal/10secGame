2c17126a3f972a5efb59940e30340bc8aa5be0e5
Komkanit <kom.sujautra@gmail.com>
2015-06-21 01:09:47 +0700

    fix some bug

--- script.js
+++ script.js
@@ -1,45 +1,42 @@
 var minLengthX = 309;
 var maxLengthX = 309+500;
 var minLengthY = 21+44.88;
-var maxLengthY = 21+500+44.88;
-var numMachine = 1;
-var time = 10;
-var tt = [0,200,500,800];
-var score = 0;
-var IntervalId;
-var game = 0;
-	var box = new MyObject(x = minLengthX+240,y = minLengthY+240,length = 25);
+var maxLengthY = 21+500+44.88,
+	numMachine = 1,
+	time = 10,
+	tt = [0,200,500,800],
+	score = 0,
+	IntervalId,
+	game = 0;
+	var box = new MyObject(x = minLengthX+240,y = minLengthY+240);
 	var Mac = new Machine();
-	var map = new MyMap();
-
-
+	
 $(document).ready(function(){
-	
-	map.CreateMap();
-	Mac.check = 0;
 	document.getElementById("counter").value=time;
-	
-	IntervalId = window.setInterval(function(){
-  		box.checkhit();
-	}, 5);
-	
 	$(".box").click(function(){
 		startgame();
+		IntervalId = window.setInterval(function(){
+  		box.checkhit();
+		}, 5);
 	});
 	
 });
 function startgame(){
-	game = 1;
-	$(".text").css('font-size','0px');
-	$(".map").css('cursor','none');
-	if(Mac.check == 0){
+	console.log(game);
+	if(game == 0){
+		game = 1;
+		$(".text").css('font-size','0px');
+		$(".map").css('cursor','none');
 		Mac.CreateMac(numMachine);
-    	settime();
-		Mac.check = 1;
+		setTimeout(function(){
+			Mac.Macmove(numMachine);
+			settime();
+		},500);   	
+		$(".map").mousemove(function(e){
+			box.moveto(e.pageX,e.pageY);
+  		});	
 	}
-	$(".map").mousemove(function(e){
-		box.moveto(e.pageX,e.pageY);
-  	});
+  	
 }
 function alertbox(word){
 	$(".massbox").remove();
@@ -58,6 +55,7 @@ function endgame(){
 	$(".textbut").on('click',function(){
 	location.reload();	
 	});
+	
 }
 function nextstage(){
 	score+=5;
@@ -70,17 +68,30 @@ function nextstage(){
 	});
 }
 
-function MyObject(x,y,length){
+function reset(){
+	window.clearInterval(IntervalId);
+	$(".Mac").remove();
+	$(".level").remove();
+	var div = "<div class = 'level'>Level "+numMachine+"</div>";
+	$(".head").before(div);
+	$(".textbox").css('display','none');
+	document.getElementById("counter").value=time;
+	$(".map").css('cursor','default');
+	box.moveto(x = minLengthX+240,y = minLengthY+240);
+ 	$(".text").css('font-size','20px');	
+ 	$(".map").off();
+ 	game = 0;
+}
+
+function MyObject(x,y){
 	this.x = x;
 	this.y = y;
-	this.length = length;
 	var div = '<div class="box"></div>';
 	$(".map").append(div);
 	$(".box").css("left",x+"px");
 	$(".box").css("top",y+"px");
 	
 	this.moveto = function(x,y){
-
 		if(y < maxLengthY && y > minLengthY){
  			$(".box").css({'top': y});
  		}
@@ -89,7 +100,6 @@ function MyObject(x,y,length){
  		}
 
 	}
-
 	this.checkhit = function(){
 		var hit_list = $(".box").collision(".Mac");
 		if(hit_list.length){
@@ -105,130 +115,80 @@ function Machine(){
 
 		for(var i = 1 ;i <= n ; i++){
 			var Class = "Mac" + " " + i;
-		var div = "<div class = '" + Class + "'></div>";
-		$(".map").append(div);
-		var x = Math.floor((Math.random() * (maxLengthX-minLengthX+1))+minLengthX);
-		var y = Math.floor((Math.random() * (maxLengthY-minLengthY+1))+minLengthY);
-		 Class = ".Mac" + "." + i;
-		 
-		$(Class).css("left",x);
-		$(Class).css("top",y);
-		move(0,i);
+			var div = "<div class = '" + Class + "'></div>";
+			$(".map").append(div);
+			do{
+				var x = Math.floor((Math.random() * (maxLengthX-minLengthX+1))+minLengthX);
+				var y = Math.floor((Math.random() * (maxLengthY-minLengthY+1))+minLengthY);
+		 		Class = ".Mac" + "." + i;
+				$(Class).css("left",x);
+				$(Class).css("top",y);
+				var hit_list = $(".box").collision(Class);
+			}while(hit_list.length == 1);
+			
 		}
-
 		
+	}
+	this.Macmove = function(n){
+		for(var i = 1 ;i <= n ; i++){
+				move(0,i);
+		}
 	}
 }
 
 function move(s,num){
 		
-		do{
-			var side =	Math.floor((Math.random() * 4)+1);	
-		}while(side == s);
-		var x;
-		var y;
-		switch(side){
-			case 1: // top
-			 x = Math.floor((Math.random() * (maxLengthX-minLengthX+1))+minLengthX);
-			 y = minLengthY;
-			 break;
+	do{
+		var side =	Math.floor((Math.random() * 4)+1);	
+	}while(side == s);
 
-			 case 2: // right
-			 x = maxLengthX;
-			 y = Math.floor((Math.random() * (maxLengthY-minLengthY+1))+minLengthY);
-			 break;
+	switch(side){
+		case 1: // top
+		var x = Math.floor((Math.random() * (maxLengthX-minLengthX+1))+minLengthX);
+		var y = minLengthY;
+		break;
 
-			 case 3: // bott
-			 x = Math.floor((Math.random() * (maxLengthX-minLengthX+1))+minLengthX);
-			 y = maxLengthY;
-			 break;
+		case 2: // right
+		var x = maxLengthX;
+		var y = Math.floor((Math.random() * (maxLengthY-minLengthY+1))+minLengthY);
+		break;
 
-			 case 4: // left
-			 x = minLengthX;
-			 y = Math.floor((Math.random() * (maxLengthY-minLengthY+1))+minLengthY);
-			 break;
+		case 3: // bott
+		var x = Math.floor((Math.random() * (maxLengthX-minLengthX+1))+minLengthX);
+		var y = maxLengthY;
+		break;
+
+		case 4: // left
+		var x = minLengthX;
+		var y = Math.floor((Math.random() * (maxLengthY-minLengthY+1))+minLengthY);
+		break;
 
 		}
 		var Class = ".Mac" + "." + num;
 		var t = Math.floor((Math.random() * 4));
-		
 			setTimeout(function(){
-			$(Class).animate({top: y+"px" , left: x+"px"},1000,function(){
-			move(side,num);
-		});
-		},tt[t]);
-		
-		
+				$(Class).animate({top: y+"px" , left: x+"px"},1000,function(){
+				move(side,num);
+				});
+			},tt[t]);		
 }
-
-
-
-
-function MyMap(){
-	this.map = [
-				[0,0,0,0,0,0,0,0,0,0],
-				[0,0,0,0,0,0,0,0,0,0],
-				[0,0,0,0,0,0,0,0,0,0],
-				[0,0,0,0,0,0,0,0,0,0],
-				[0,0,0,0,0,0,0,0,0,0],
-				[0,0,0,0,0,0,0,0,0,0],
-				[0,0,0,0,0,0,0,0,0,0],
-				[0,0,0,0,0,0,0,0,0,0],
-				[0,0,0,0,0,0,0,0,0,0],
-				[0,0,0,0,0,0,0,0,0,0],
-				[0,0,0,0,0,0,0,0,0,0],
-				[0,0,0,0,0,0,0,0,0,0]
-			  ];
-
-	this.CreateMap = function(){
-		var wall = "<div class = 'wall'></div>";
-		for(var i = 0,y = this.map.length; i < y ; i++){
-			for(var j = 0,x = this.map[0].length; j < x ; j++){
-				if(this.map[i][j] == 1){
-					$(".map").append(wall);
-					$(".wall").last().css("left",j*50);
-					$(".wall").last().css("top",i*50);
-				}
-			}
-		}
-	}
-}
-
 
 function settime(){
-
 	var seconds=time+1;
 	document.getElementById("counter").value=time;
-
 	display(time,seconds);
 }
 function display(time,seconds){
 	if(game == 0){
  		return;
  	}
+ 	
     seconds-=1;
- 	
  	document.getElementById("counter").value=seconds;
- 	
  	if(seconds==0){
  		nextstage();
  		return;
  	} 
- 	
     score++;
     setTimeout("display("+time+","+seconds+")",1000);
-}
-
-function reset(){
-	$(".Mac").remove();
-	$(".level").remove();
-	var div = "<div class = 'level'>Level "+numMachine+"</div>";
-	$(".head").before(div);
-	$(".textbox").css('display','none');
-	document.getElementById("counter").value=time;
-	$(".map").css('cursor','default');
-	box.moveto(x = minLengthX+240,y = minLengthY+240);
- 	$(".text").css('font-size','20px');	
- 	$(".map").off();
- 	Mac.check = 0;
 }
